00e18541ce2cced3d2a96cfecbcec6294bd7242f
kyj-dot <kuyouji987@163.com>
2020-07-24 13:24:16 +0800

    父工程,commmon,oauth添加jar包

---
+++ cooltiger-oauth/src/main/java/com/cool/oauth/controller/OauthController.java
@@ -0,0 +1,12 @@
+package com.cool.oauth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author guoxq
+ * @version 1.0
+ * @date 2020/7/23 16:58
+ */
+@RestController
+public class OauthController {
+}
