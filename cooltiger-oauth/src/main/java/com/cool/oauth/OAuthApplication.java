7e26d867457f44613f25f38f2318021aca33c494
kyj-dot <kuyouji987@163.com>
2020-07-27 10:00:34 +0800

    修改多余的jar

--- cooltiger-oauth/src/main/java/com/cool/oauth/OAuthApplication.java
+++ cooltiger-oauth/src/main/java/com/cool/oauth/OAuthApplication.java
@@ -1,16 +1,13 @@
 package com.cool.oauth;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author guoxq
  * @version 1.0
  * @date 2020/7/23 15:28
  */
-@SpringBootApplication
+
 public class OAuthApplication {
-  public static void main(String[] args) {
+ /* public static void main(String[] args) {
         SpringApplication.run(OAuthApplication.class,args);
-    }
+    }*/
 }
