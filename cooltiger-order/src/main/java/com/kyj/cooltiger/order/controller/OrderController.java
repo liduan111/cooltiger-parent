2943d223725615978c0fce916c237c63d3279293
kyj-dot <kuyouji987@163.com>
2020-08-17 17:00:35 +0800

    提交订单服务模板

--- cooltiger-order/src/main/java/com/kyj/cooltiger/order/controller/OrderController.java
+++ cooltiger-order/src/main/java/com/kyj/cooltiger/order/controller/OrderController.java
@@ -1,9 +1,14 @@
 package com.kyj.cooltiger.order.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author guoxq
  * @version 1.0
  * @date 2020/8/17 16:09
  */
+@RestController
+@RequestMapping(value = "/order")
 public class OrderController {
 }
---
+++ cooltiger-order/src/main/java/com/kyj/cooltiger/order/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package com.kyj.cooltiger.order.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author guoxq
+ * @version 1.0
+ * @date 2020/8/17 16:57
+ */
+@Mapper
+public interface OrderMapper {
+}
